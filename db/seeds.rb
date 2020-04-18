c3536564be153dbd0285d6395b9d68cf7059dd6c
Alexis Lenoir <alexis59495@gmail.com>
2020-04-18 16:31:53 -0300

    Do the exercise without CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(0 .. 10).each do |x|
+    title = Faker::Name.first_name
+    content = Faker::Name.name
+    article = Article.create(title: title, content: content)
+end
